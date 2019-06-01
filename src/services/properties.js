4159742aa17d33bfdaddefdc0d79d875a80806cd
Brennedith Garcia <brennedithg@gmail.com>
2019-06-01 16:16:52 -0500

    1. Configure json-server to mockup a RESTfull api 2. Create the base properties service to work with backend

---
+++ src/services/properties.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const endpoint =
+  process.env.NODE_ENV === 'production' ? 'TBD' : 'http://localhost:5000/';
+
+class PropertiesService {
+  service = axios.create({
+    baseURL: endpoint,
+    timeout: 1000
+  });
+
+  create(body) {
+    const path = '/';
+    return this.service.post(path, body);
+  }
+
+  getAll() {
+    const path = '/';
+    return this.service.get(path);
+  }
+  get(id) {
+    const path = `/${id}`;
+    return this.service.get(path);
+  }
+
+  update(id, body) {
+    const path = `/${id}`;
+    return this.service.patch(path, body);
+  }
+
+  delete(id) {
+    const path = `/${id}`;
+    return this.service.delete(path);
+  }
+}
+
+const propertiesService = new PropertiesService();
+
+export default propertiesService;
