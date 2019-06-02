71dd94cacc36bc17fd0c7e25b0c87ee39cf099a7
Brennedith Garcia <brennedithg@gmail.com>
2019-06-02 10:38:52 -0500

    Add price information

--- src/pages/components/Properties/List.js
+++ src/pages/components/Properties/List.js
@@ -4,7 +4,11 @@ import AppContext from '../../../services/context';
 
 import './List.css';
 
-const PropertyRow = ({ id, details, owner }) => {
+const PropertyRow = ({ property }) => {
+  const { id, details, owner, price } = property;
+
+  const priceTag = `$${price.toLocaleString()}`;
+
   const editUrl = `/edit/${id}`;
   const redirectToEdit = e => {
     e.preventDefault();
@@ -19,6 +23,7 @@ const PropertyRow = ({ id, details, owner }) => {
       <td>{owner.name}</td>
       <td>{owner.email}</td>
       <td>{owner.phone}</td>
+      <td>{priceTag}</td>
       <td>
         <a href={editUrl}>Edit</a>
       </td>
@@ -31,7 +36,7 @@ const Properties = () => {
   const { filteredProperties } = state;
 
   const propertiesRows = filteredProperties.map(property => (
-    <PropertyRow key={property.id} {...property} />
+    <PropertyRow key={property.id} property={property} />
   ));
 
   return (
@@ -48,6 +53,7 @@ const Properties = () => {
           <th className="column-division">Name</th>
           <th>Email</th>
           <th>Phone Number</th>
+          <th className="column-division">Price</th>
         </tr>
       </thead>
       <tbody>{propertiesRows}</tbody>
